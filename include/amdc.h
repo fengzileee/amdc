3240f61c20e35ff24fb1a964b42dacaa6b8d369c
Linfeng <linfengleee@gmail.com>
2017-07-05 13:53:25 +0800

    Add subscriber for the robot to update its state (state in a control theory sense).

--- include/amdc.h
+++ include/amdc.h
@@ -3,6 +3,7 @@
 
 #include <queue>
 #include <Eigen/Dense>
+#include <cmath>
 
 struct PropellerCommand
 {
@@ -35,6 +36,30 @@ public:
         range.resize(7);
         debris_coord.resize(2);
     }
+
+    void stateUpdateCallback(const nav_msgs::Odometry::ConstPtr& msg)
+    {
+        // ================================================
+        // =    Update the state of the robot through a ROS
+        // =        message with type nav_msgs/Odometry.
+        // ================================================
+
+        // =========== position: x, y, theta =============
+        state(0) = msg -> PoseWithCovariance.pose.position.x; // x
+        state(1) = msg -> PoseWithCovariance.pose.position.y; // y
+        // rotation about z
+        float qx = msg -> PoseWithCovariance.pose.orientation.x;
+        float qy = msg -> PoseWithCovariance.pose.orientation.y;
+        float qz = msg -> PoseWithCovariance.pose.orientation.z;
+        float qw = msg -> PoseWithCovariance.pose.orientation.w;
+        state(2) = std::atan2(2 * (qw * qz + qx * qy), 
+                1 - 2 * (qy * qy + qz * qz));
+        // ========== velocity: time derivative of position ============
+        state(3) = msg -> TwistWithCovariance.twist.linear.x; 
+        state(4) = msg -> TwistWithCovariance.twist.linear.y; 
+        state(5) = msg -> TwistWithCovariance.twist.angular.z; 
+    }
+
 };
 
 extern Amdc amdc_s;
