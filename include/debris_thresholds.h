6ff79be3988d4f53c8ab97268412d1a7b0f8c046
randolph <wwk.randolph@gmail.com>
2017-08-02 19:01:23 +0800

    Finetune debris threshold

--- include/debris_thresholds.h
+++ include/debris_thresholds.h
@@ -1,23 +1,23 @@
 #ifndef debris_thresholds__H
 #define debris_thresholds__H
 
-const float open_door_box_x1 = 0.4;
-const float open_door_box_y1 = 0.4;
-const float open_door_box_x2 = 0.6;
-const float open_door_box_y2 = 0.6;
+const float open_door_box_x1 = 0.25;
+const float open_door_box_y1 = 0.1;
+const float open_door_box_x2 = 0.75;
+const float open_door_box_y2 = 0.5;
 const float open_door_box_w = open_door_box_x2 - open_door_box_x1;
 const float open_door_box_h = open_door_box_y2 - open_door_box_y1;
 
-const float close_door_box_x1 = 0.1;
-const float close_door_box_y1 = 0.8;
-const float close_door_box_x2 = 0.9;
+const float close_door_box_x1 = 0.0;
+const float close_door_box_y1 = 1.0;
+const float close_door_box_x2 = 1.0;
 const float close_door_box_y2 = 1.0;
 const float close_door_box_w = close_door_box_x2 - close_door_box_x1;
 const float close_door_box_h = close_door_box_y2 - close_door_box_y1;
 
-const float stay_opened_box_x1 = 0.20;
-const float stay_opened_box_y1 = 0.2;
-const float stay_opened_box_x2 = 0.8;
+const float stay_opened_box_x1 = 0.0;
+const float stay_opened_box_y1 = 0.0;
+const float stay_opened_box_x2 = 1.0;
 const float stay_opened_box_y2 = 1.0;
 const float stay_opened_box_w = stay_opened_box_x2 - stay_opened_box_x1;
 const float stay_opened_box_h = stay_opened_box_y2 - stay_opened_box_y1;
